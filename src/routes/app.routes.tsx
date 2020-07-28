fa3f2233e9740925eb11fc12467ae8870e7fe5d2
Maick Souza <maick_a_s@msn.com>
2020-07-27 21:24:52 -0300

    chore(mobile): routes create

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -17,9 +17,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
-    <App.Screen name="Profile" component={Profile} />
     <App.Screen name="CreateAppointment" component={CreateAppointment} />
     <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
